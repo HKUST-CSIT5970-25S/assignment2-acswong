c392cbeba6e85b674c15aebab5e061edce1b8589
acswong <acswong@connect.ust.hk>
2025-03-31 20:47:37 +0800

    Update BigramFrequencyStripes.java

--- src/main/java/hk/ust/csit5970/BigramFrequencyStripes.java
+++ src/main/java/hk/ust/csit5970/BigramFrequencyStripes.java
@@ -29,165 +29,153 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
 public class BigramFrequencyStripes extends Configured implements Tool {
-	private static final Logger LOG = Logger
-			.getLogger(BigramFrequencyStripes.class);
+    private static final Logger LOG = Logger.getLogger(BigramFrequencyStripes.class);
 
-	private static class MyMapper extends
-			Mapper<LongWritable, Text, Text, HashMapStringIntWritable> {
-		private static final Text KEY = new Text();
-		private static final HashMapStringIntWritable STRIPE = new HashMapStringIntWritable();
+    private static class MyMapper extends
+            Mapper<LongWritable, Text, Text, HashMapStringIntWritable> {
+        private static final Text KEY = new Text();
+        private static final HashMapStringIntWritable STRIPE = new HashMapStringIntWritable();
 
-		@Override
-		public void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
-			String line = ((Text) value).toString();
-			String[] words = line.trim().split("\\s+");
+        @Override
+        public void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] words = line.trim().split("\\s+");
 
-			if (words.length < 2) {
-				return;
-			}
+            if (words.length < 2) {
+                return;
+            }
 
-			for (int i = 0; i < words.length - 1; i++) {
-				String currentWord = words[i];
-				String nextWord = words[i+1];
-				
-				STRIPE.clear();
-				STRIPE.increment(nextWord);
-				
-				KEY.set(currentWord);
-				context.write(KEY, STRIPE);
-			}
-		}
-	}
+            for (int i = 0; i < words.length - 1; i++) {
+                STRIPE.clear();
+                STRIPE.increment(words[i+1]);
+                KEY.set(words[i]);
+                context.write(KEY, STRIPE);
+            }
+        }
+    }
 
-	private static class MyReducer extends
-			Reducer<Text, HashMapStringIntWritable, PairOfStrings, FloatWritable> {
-		private final static HashMapStringIntWritable SUM_STRIPES = new HashMapStringIntWritable();
-		private final static PairOfStrings BIGRAM = new PairOfStrings();
-		private final static FloatWritable FREQ = new FloatWritable();
+    private static class MyCombiner extends
+            Reducer<Text, HashMapStringIntWritable, Text, HashMapStringIntWritable> {
+        private final static HashMapStringIntWritable SUM_STRIPES = new HashMapStringIntWritable();
 
-		@Override
-		public void reduce(Text key,
-				Iterable<HashMapStringIntWritable> stripes, Context context)
-				throws IOException, InterruptedException {
-			SUM_STRIPES.clear();
-			for (HashMapStringIntWritable stripe : stripes) {
-				SUM_STRIPES.plus(stripe);
-			}
+        @Override
+        public void reduce(Text key, Iterable<HashMapStringIntWritable> stripes, 
+                Context context) throws IOException, InterruptedException {
+            SUM_STRIPES.clear();
+            for (HashMapStringIntWritable stripe : stripes) {
+                SUM_STRIPES.plus(stripe);
+            }
+            context.write(key, SUM_STRIPES);
+        }
+    }
 
-			float total = 0;
-			for (int count : SUM_STRIPES.values()) {
-				total += count;
-			}
+    private static class MyReducer extends
+            Reducer<Text, HashMapStringIntWritable, PairOfStrings, FloatWritable> {
+        private final static HashMapStringIntWritable SUM_STRIPES = new HashMapStringIntWritable();
+        private final static PairOfStrings BIGRAM = new PairOfStrings();
+        private final static FloatWritable FREQ = new FloatWritable();
 
-			BIGRAM.set(key.toString(), "");
-			FREQ.set(total);
-			context.write(BIGRAM, FREQ);
+        @Override
+        public void reduce(Text key, Iterable<HashMapStringIntWritable> stripes, 
+                Context context) throws IOException, InterruptedException {
+            SUM_STRIPES.clear();
+            for (HashMapStringIntWritable stripe : stripes) {
+                SUM_STRIPES.plus(stripe);
+            }
 
-			for (Map.Entry<String, Integer> entry : SUM_STRIPES.entrySet()) {
-				BIGRAM.set(key.toString(), entry.getKey());
-				FREQ.set(entry.getValue() / total);
-				context.write(BIGRAM, FREQ);
-			}
-		}
-	}
+            float total = 0;
+            for (int count : SUM_STRIPES.values()) {
+                total += count;
+            }
 
-	private static class MyCombiner
-			extends
-			Reducer<Text, HashMapStringIntWritable, Text, HashMapStringIntWritable> {
-		private final static HashMapStringIntWritable SUM_STRIPES = new HashMapStringIntWritable();
+            BIGRAM.set(key.toString(), "");
+            FREQ.set(total);
+            context.write(BIGRAM, FREQ);
 
-		@Override
-		public void reduce(Text key,
-				Iterable<HashMapStringIntWritable> stripes, Context context)
-				throws IOException, InterruptedException {
-			SUM_STRIPES.clear();
-			for (HashMapStringIntWritable stripe : stripes) {
-				SUM_STRIPES.plus(stripe);
-			}
-			context.write(key, SUM_STRIPES);
-		}
-	}
+            for (Map.Entry<String, Integer> entry : SUM_STRIPES.entrySet()) {
+                if (entry.getValue() > 1) {
+                    BIGRAM.set(key.toString(), entry.getKey());
+                    FREQ.set(entry.getValue() / total);
+                    context.write(BIGRAM, FREQ);
+                }
+            }
+        }
+    }
 
-	public BigramFrequencyStripes() {
-	}
+    public BigramFrequencyStripes() {}
 
-	private static final String INPUT = "input";
-	private static final String OUTPUT = "output";
-	private static final String NUM_REDUCERS = "numReducers";
+    private static final String INPUT = "input";
+    private static final String OUTPUT = "output";
+    private static final String NUM_REDUCERS = "numReducers";
 
-	@SuppressWarnings({ "static-access" })
-	public int run(String[] args) throws Exception {
-		Options options = new Options();
+    @SuppressWarnings("static-access")
+    public int run(String[] args) throws Exception {
+        Options options = new Options();
+        options.addOption(OptionBuilder.withArgName("path").hasArg()
+                .withDescription("input path").create(INPUT));
+        options.addOption(OptionBuilder.withArgName("path").hasArg()
+                .withDescription("output path").create(OUTPUT));
+        options.addOption(OptionBuilder.withArgName("num").hasArg()
+                .withDescription("number of reducers").create(NUM_REDUCERS));
 
-		options.addOption(OptionBuilder.withArgName("path").hasArg()
-				.withDescription("input path").create(INPUT));
-		options.addOption(OptionBuilder.withArgName("path").hasArg()
-				.withDescription("output path").create(OUTPUT));
-		options.addOption(OptionBuilder.withArgName("num").hasArg()
-				.withDescription("number of reducers").create(NUM_REDUCERS));
+        CommandLine cmdline;
+        CommandLineParser parser = new GnuParser();
+        try {
+            cmdline = parser.parse(options, args);
+        } catch (ParseException exp) {
+            System.err.println("Error parsing command line: " + exp.getMessage());
+            return -1;
+        }
 
-		CommandLine cmdline;
-		CommandLineParser parser = new GnuParser();
+        if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
+            System.out.println("args: " + Arrays.toString(args));
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.setWidth(120);
+            formatter.printHelp(this.getClass().getName(), options);
+            ToolRunner.printGenericCommandUsage(System.out);
+            return -1;
+        }
 
-		try {
-			cmdline = parser.parse(options, args);
-		} catch (ParseException exp) {
-			System.err.println("Error parsing command line: "
-					+ exp.getMessage());
-			return -1;
-		}
+        String inputPath = cmdline.getOptionValue(INPUT);
+        String outputPath = cmdline.getOptionValue(OUTPUT);
+        int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ? 
+                Integer.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
 
-		if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
-			System.out.println("args: " + Arrays.toString(args));
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.setWidth(120);
-			formatter.printHelp(this.getClass().getName(), options);
-			ToolRunner.printGenericCommandUsage(System.out);
-			return -1;
-		}
+        LOG.info("Tool: " + BigramFrequencyStripes.class.getSimpleName());
+        LOG.info(" - input path: " + inputPath);
+        LOG.info(" - output path: " + outputPath);
+        LOG.info(" - number of reducers: " + reduceTasks);
 
-		String inputPath = cmdline.getOptionValue(INPUT);
-		String outputPath = cmdline.getOptionValue(OUTPUT);
-		int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ? Integer
-				.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
+        Configuration conf = getConf();
+        Job job = Job.getInstance(conf);
+        job.setJobName(BigramFrequencyStripes.class.getSimpleName());
+        job.setJarByClass(BigramFrequencyStripes.class);
+        job.setNumReduceTasks(reduceTasks);
 
-		LOG.info("Tool: " + BigramFrequencyStripes.class.getSimpleName());
-		LOG.info(" - input path: " + inputPath);
-		LOG.info(" - output path: " + outputPath);
-		LOG.info(" - number of reducers: " + reduceTasks);
+        FileInputFormat.setInputPaths(job, new Path(inputPath));
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
-		Configuration conf = getConf();
-		Job job = Job.getInstance(conf);
-		job.setJobName(BigramFrequencyStripes.class.getSimpleName());
-		job.setJarByClass(BigramFrequencyStripes.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(HashMapStringIntWritable.class);
+        job.setOutputKeyClass(PairOfStrings.class);
+        job.setOutputValueClass(FloatWritable.class);
 
-		job.setNumReduceTasks(reduceTasks);
+        job.setMapperClass(MyMapper.class);
+        job.setCombinerClass(MyCombiner.class);
+        job.setReducerClass(MyReducer.class);
 
-		FileInputFormat.setInputPaths(job, new Path(inputPath));
-		FileOutputFormat.setOutputPath(job, new Path(outputPath));
+        Path outputDir = new Path(outputPath);
+        FileSystem.get(conf).delete(outputDir, true);
 
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(HashMapStringIntWritable.class);
-		job.setOutputKeyClass(PairOfStrings.class);
-		job.setOutputValueClass(FloatWritable.class);
+        long startTime = System.currentTimeMillis();
+        job.waitForCompletion(true);
+        LOG.info("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
 
-		job.setMapperClass(MyMapper.class);
-		job.setCombinerClass(MyCombiner.class);
-		job.setReducerClass(MyReducer.class);
+        return 0;
+    }
 
-		Path outputDir = new Path(outputPath);
-		FileSystem.get(conf).delete(outputDir, true);
-
-		long startTime = System.currentTimeMillis();
-		job.waitForCompletion(true);
-		LOG.info("Job Finished in " + (System.currentTimeMillis() - startTime)
-				/ 1000.0 + " seconds");
-
-		return 0;
-	}
-
-	public static void main(String[] args) throws Exception {
-		ToolRunner.run(new BigramFrequencyStripes(), args);
-	}
+    public static void main(String[] args) throws Exception {
+        ToolRunner.run(new BigramFrequencyStripes(), args);
+    }
 }
